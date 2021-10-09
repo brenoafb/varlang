8f8a474d1f9b2f5e017a331837ae36007ddd7da5
breno.afb <breno.afb@gmail.com>
2021-10-09 19:19:27 -0300

    Add variational syntax, variational program example

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,45 @@ import Language.Syntax
 import Language.Parser
 import Text.Pretty.Simple (pPrint)
 
+program =
+  [ Decl "twice" [ "x" ]
+      ( Mult
+          { e1 = Num 2
+          , e2 = Var "x"
+          }
+      )
+  , Decl "main" []
+      ( FunCall "twice"
+          [ Num 2 ]
+      )
+  ]
+
+twice =
+  Decl "twice" [ "x" ]
+    ( Mult
+        { e1 = Num 2
+        , e2 = Var "x"
+        }
+    )
+
+twice' =
+  Decl "twice" [ "x" ]
+    ( Add
+        { e1 = Var "x"
+        , e2 = Var "x"
+        }
+    )
+
+vtwice = Dim "Impl" ["plus", "times"]
+  $ Obj (Decl "twice" [ "x" ] i)
+  where i = choice "Impl" [
+              ( Add  { e1 = Var "x", e2 = Var "x" }),
+              ( Mult { e1 = Num 2, e2 = Var "x" })
+            ]
+
+choice :: Dim -> [Expr] -> Expr
+choice d es = VExpr $ Chc d (map Obj es)
+
 main = do
   src <- getContents
   pPrint $ parseStr src
--- src/Language/Syntax.hs
+++ src/Language/Syntax.hs
@@ -3,6 +3,7 @@
 module Language.Syntax where
 
 import Data.Data
+import Data.Var
 
 type Program = [Decl]
 
@@ -11,12 +12,15 @@ type Ident = String
 data Decl = Decl Ident [Ident] Expr
   deriving (Show, Data)
 
+type VExpr = V Expr
+
 data Expr = Num Int
           | Var Ident
           | Neg Expr
           | FunCall Ident [Expr]
-          | Add {e1 :: Expr, e2 :: Expr}
-          | Sub {e1 :: Expr, e2 :: Expr}
-          | Mult {e1 :: Expr, e2 :: Expr}
-          | Div {e1 :: Expr, e2 :: Expr}
+          | Add  { e1 :: Expr, e2 :: Expr }
+          | Sub  { e1 :: Expr, e2 :: Expr }
+          | Mult { e1 :: Expr, e2 :: Expr }
+          | Div  { e1 :: Expr, e2 :: Expr }
+          | VExpr VExpr
           deriving (Show, Data)
